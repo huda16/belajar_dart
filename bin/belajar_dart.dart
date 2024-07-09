07ad701d378eeca6aa1a764adb606bc87d024783
Mochamad Nurul Huda <mochamadnurulhuda16@gmail.com>
2024-07-09 13:27:12 +0700

    comments

--- bin/belajar_dart.dart
+++ bin/belajar_dart.dart
@@ -1,5 +1,13 @@
 import 'package:belajar_dart/belajar_dart.dart' as belajar_dart;
 
 void main(List<String> arguments) {
+  // Single line comment
+
+  /*
+  multi
+  line
+  comment
+ */
+
   print('Hello world: ${belajar_dart.calculate()}!');
 }
