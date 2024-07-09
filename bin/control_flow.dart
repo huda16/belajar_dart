6fa9d4293cb3df8cce29a4a03e19c8b1380ae6cc
Mochamad Nurul Huda <mochamadnurulhuda16@gmail.com>
2024-07-09 18:23:47 +0700

    switch and case

--- bin/control_flow.dart
+++ bin/control_flow.dart
@@ -2,6 +2,7 @@ import 'control_flow/if_else.dart';
 import 'control_flow/for_loops.dart';
 import 'control_flow/while_and_dowhile.dart';
 import 'control_flow/break_continue.dart';
+import 'control_flow/switch_case.dart';
 
 void controlFlow() {
   print('Control Flow');
@@ -13,5 +14,7 @@ void controlFlow() {
   whileAndDowhile();
   print('\n');
   breakContinue();
+  print('\n');
+  switchCase();
   print('\n');
 }
